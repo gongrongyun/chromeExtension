79b37636ae0e279d22ad680508da8a30c8d3994b
gongrongyun <1716175849@qq.com>
2019-04-11 00:14:14 +0800

    add Promise

--- app/js/background.js
+++ app/js/background.js
@@ -1,5 +1,28 @@
 var isWorking = false;
 
+
+function httpRequest(url, data) {
+    const promise = new Promise(function(resolve, reject) {
+        const handler = function() {
+            if(this.readyState != 4) {
+                return;
+            }
+            if(this.status === 200) {
+                resolve(this.response);
+            } else {
+                reject(new Error(this.statusText));
+            }
+        }
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", url);
+        xhr.onreadystatechange = handler;
+        xhr.send(JSON.stringify(data));
+
+    });
+
+    return promise;
+}
+
 function sendMessageToContentScript(message, callback) {
     chrome.tabs.query({active: true, currentWindow:true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
@@ -38,6 +61,7 @@ function end() {
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     //将数据发往后端
+    // httpRequest(url, request);
     chrome.notifications.create(null, {
         type: 'basic',
         iconUrl: 'app/images/logo_v2_qkteam.png',
